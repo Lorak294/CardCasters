a472e95e29a30c17fece5d08a64cde428300e312
Karol Kącki <karol.kacki2@gmail.com>
2023-10-11 11:16:05 +0200

    cleanup

--- src/routes/api/login/+server.ts
+++
@@ -1,9 +0,0 @@
-import db from '$lib/database/database';
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	// TEMPORARY IMPLEMENTATION: returns first user from the db
-	const res = await db.user.findFirst();
-	return json(res);
-};
